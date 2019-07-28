95e3e8b30a6002fe719c0a7b7c2770b6e0565d47
Junrae Mon Collano <junraemon@gmail.com>
2019-07-28 19:22:46 +0800

    feat(core): added firebase deploy

--- apps/filipino-food-delicacies/src/app/app.module.ts
+++ apps/filipino-food-delicacies/src/app/app.module.ts
@@ -1,19 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import {
-  FirebaseModule,
-  AngularFireDatabaseModule,
-  AngularFireAuthModule
-} from './firebase/firebase.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    FirebaseModule,
+    AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule
   ],
--- apps/filipino-food-delicacies/src/app/firebase/firebase.module.ts
+++
@@ -1,7 +0,0 @@
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { environment } from '../../environments/environment';
-
-export const FirebaseModule = AngularFireModule.initializeApp(environment);
-export { AngularFireDatabaseModule, AngularFireAuthModule };
